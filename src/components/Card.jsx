def2e44cba04e154069033806af62947c9334250
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-23 10:50:56 +0400

    feat: render image with correct dimensions

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,16 @@
 export default function Card({ product }) {
   const { title, price, description, image, badge } = product;
   return (
-    <>
+    <div className="max-w-80">
+      <img
+        src={image}
+        alt={`pic-${title}`}
+        className="w-full h-52 object-cover"
+      />
       <h1>{title}</h1>
       <p>{description}</p>
       <div>{price}</div>
       <div>{badge}</div>
-      <img src={image} alt={`pic-${title}`} />
-    </>
+    </div>
   );
 }
