1a3a30be08cf3ffbf760d1c5dc6519d8859d38e5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-23 11:07:33 +0400

    style: format card title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,15 @@
 export default function Card({ product }) {
   const { title, price, description, image, badge } = product;
   return (
-    <div className="max-w-80">
+    <div className="max-w-80 flex flex-col items-start">
       <img
         src={image}
         alt={`pic-${title}`}
         className="w-full h-52 object-cover"
       />
-      <h1>{title}</h1>
+      <h1 className="text-lg font-bold">{title}</h1>
       <p>{description}</p>
-      <div>{price}</div>
+      <div className="text-xl font-bold">{price}</div>
       <div>{badge}</div>
     </div>
   );
