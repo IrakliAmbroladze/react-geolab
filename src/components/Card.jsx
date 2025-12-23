3c203353f9a35d4aaaf6c1614cd2409759a751cd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-23 11:23:44 +0400

    feat: add button for add to cart

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ product }) {
   const { title, price, description, image, badge } = product;
   return (
-    <div className="max-w-80 flex flex-col items-start">
+    <div className="max-w-80 flex flex-col items-start gap-1">
       <img
         src={image}
         alt={`pic-${title}`}
@@ -9,7 +9,12 @@ export default function Card({ product }) {
       />
       <h1 className="text-lg font-bold">{title}</h1>
       <p>{description}</p>
-      <div className="text-xl font-bold">{price}</div>
+      <div className="flex justify-between w-full items-center">
+        <div className="text-xl font-bold">{price}</div>
+        <button className="gap-x-1.5 p-2 px-4 bg-blue-500 rounded-md text-white text-xs font-bold cursor-pointer">
+          Add to Cart
+        </button>
+      </div>
       <div>{badge}</div>
     </div>
   );
