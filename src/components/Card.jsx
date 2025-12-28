2a69a7f36f5a2b0a1e42cf22682ca6b7b3250dce
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-12-29 00:54:50 +0400

    refac(Card): remove extra code

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,39 +1,17 @@
 export default function Card({ product }) {
-  const { title, price, description, image, badge } = product;
-  let badgeBg = "";
-  switch (badge) {
-    case "Sale":
-      badgeBg = "#fb2c36";
-      break;
-    case "New":
-      badgeBg = "green";
-      break;
-    case "Limited":
-      badgeBg = "#ad46ff";
-  }
+  const { title, quantity, thumbnail } = product;
   return (
     <div className="w-80 flex flex-col items-start gap-1 hover:shadow-md">
       <div className="relative w-full">
         <img
-          src={image}
+          src={thumbnail}
           alt={`pic-${title}`}
           className="w-full h-52 object-cover relative"
         />
-        {!!badge && (
-          <div
-            style={{
-              backgroundColor: badgeBg,
-            }}
-            className="absolute left-2 top-2 text-white px-2 py-0.5 text-xs font-bold rounded-xl"
-          >
-            {badge}
-          </div>
-        )}
       </div>
       <h1 className="text-lg font-bold">{title}</h1>
-      <p>{description}</p>
       <div className="flex justify-between w-full items-center">
-        <div className="text-xl font-bold">{price}</div>
+        <div className="text-xl font-bold">{quantity}</div>
         <button className="gap-x-1.5 p-2 px-4 bg-blue-500 rounded-md text-white text-xs font-bold cursor-pointer">
           Add to Cart
         </button>
