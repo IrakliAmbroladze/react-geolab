34e134d8c2e355453de53722fe28fc5d5de2117f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-07 20:59:35 +0400

    feat: change UI based on new products data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,21 @@
 import { Button } from "./Button";
 
 export default function Card({ product, handleBtnClick }) {
-  const { id, title, quantity, thumbnail } = product;
+  const { id, name, /*quantity,*/ description } = product;
+
   return (
     <div className="w-80 flex flex-col items-start gap-2 hover:shadow-md hover:scale-102 p-2 rounded-lg justify-between bg-white transition-all duration-300 ease-in-out">
-      <div className="relative w-full">
+      {/* <div className="relative w-full">
         <img
           src={thumbnail}
-          alt={`pic-${title}`}
+          alt={`pic-${name}`}
           className="w-full h-52 object-cover relative"
         />
-      </div>
-      <h1 className="text-sm font-bold">{title}</h1>
+      </div>*/}
+      <h1 className="text-sm font-bold">{name}</h1>
+      <h2 className="text-sm font-bold">{description}</h2>
       <div className="flex justify-between w-full items-center">
-        <Button
+        {/*  <Button
           textContent="-"
           bgColor="skyblue"
           handleClick={() => handleBtnClick({ id, status: "decrease" })}
@@ -23,7 +25,7 @@ export default function Card({ product, handleBtnClick }) {
           textContent="+"
           bgColor="skyblue"
           handleClick={() => handleBtnClick({ id, status: "increase" })}
-        />
+        />*/}
         <Button
           textContent="delete"
           bgColor="tomato"
