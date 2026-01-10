08af87f7637af4fac5168a43a06857a3e67cbb25
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-10 15:09:30 +0400

    feat(Header): add navigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
+  const header_navs = [
+    { display_name: "Home", route_name: "/" },
+    { display_name: "Profile", route_name: "/profile" },
+    { display_name: "Settings", route_name: "/settings" },
+  ];
   return (
     <header>
-      <div>Header content.</div>
+      <Link to="/">Logo</Link>
+      <nav>
+        <ul>
+          {header_navs.map((nav, index) => (
+            <li key={index}>
+              <Link to={nav.route_name}>{nav.display_name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
