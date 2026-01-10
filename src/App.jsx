49850e8485e90e66667eaffc958ae1c731cba19d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-10 14:27:04 +0400

    feat: add Header and Fotter components in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Settings from "./pages/Settings";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/settings" element={<Settings />} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div>
+      <div>All Copyright (c) 2026 Irakli Ambroladze. All Rights Reserved.</div>
+    </div>
+  );
+}
