e9c797dff19ac7211ac107a4fdbcfab4ff7791e3
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-19 16:04:28 +0400

    feat: add delete button. need to ask about axios endpoint

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,28 +1,52 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button } from "./Button";
 
 export const Comments = ({ postId }) => {
   const [comments, setComments] = useState([]);
 
   useEffect(() => {
     const fetchComments = async () => {
-      const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
-      );
-      setComments(data);
+      try {
+        const { data } = await axios.get(
+          `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
+        );
+        setComments(data);
+      } catch (error) {
+        console.log(error);
+      }
     };
     fetchComments();
   }, [postId]);
 
+  const deletePost = async (id) => {
+    try {
+      /* await axios.delete(
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
+      );*/
+      setComments((prev) => prev.filter((comment) => comment.id !== id));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h3>These are comments</h3>
-      {comments.map((comment) => (
-        <div key={comment.id}>
-          <h4>{comment.name}</h4>
-          <p>{comment.body}</p>
-        </div>
-      ))}
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            <h4>{comment.name}</h4>
+            <p>{comment.body}</p>
+            <Button
+              className="border cursor-pointer rounded-md p-2 bg-red-800"
+              handleClick={() => deletePost(comment.id)}
+              bgColor="tomato"
+              textContent="delete"
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
