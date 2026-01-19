c3ca9c5d8e8c5aa2d0136d3c51a9e627f3173535
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-20 03:07:32 +0400

    feat: add loader spinner

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -6,6 +6,7 @@ import { CommentInputForm } from "./CommentInputForm";
 export const Comments = ({ postId }) => {
   console.log("render Comments component");
   const [comments, setComments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const commentRef = useRef(null);
 
   useEffect(() => {
@@ -15,6 +16,7 @@ export const Comments = ({ postId }) => {
           `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
         );
         setComments(data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -47,25 +49,36 @@ export const Comments = ({ postId }) => {
 
   return (
     <div>
-      <h3 className="text-2xl font-extrabold tracking-tight text-stone-400 ">
-        Comments
-      </h3>
-      <CommentInputForm createComment={createComment} ref={commentRef} />
-      <ul className="bg-gray-200 p-2.5 rounded-md flex flex-col gap-2.5 mt-5">
-        {comments.map((comment) => (
-          <li
-            key={comment.id}
-            className="bg-stone-100 p-2.5 flex justify-between items-center gap-2"
-          >
-            <p>{comment.body}</p>
-            <Button
-              handleClick={() => deletePost(comment.id)}
-              bgColor="tomato"
-              textContent="delete"
-            />
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <img
+          src={"/assets/loading.gif"}
+          alt="loading..."
+          width={50}
+          height={50}
+        />
+      ) : (
+        <>
+          <h3 className="text-2xl font-extrabold tracking-tight text-stone-400 ">
+            Comments
+          </h3>
+          <CommentInputForm createComment={createComment} ref={commentRef} />
+          <ul className="bg-gray-200 p-2.5 rounded-md flex flex-col gap-2.5 mt-5">
+            {comments.map((comment) => (
+              <li
+                key={comment.id}
+                className="bg-stone-100 p-2.5 flex justify-between items-center gap-2"
+              >
+                <p>{comment.body}</p>
+                <Button
+                  handleClick={() => deletePost(comment.id)}
+                  bgColor="tomato"
+                  textContent="delete"
+                />
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
