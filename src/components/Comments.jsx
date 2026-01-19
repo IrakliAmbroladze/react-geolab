bfa345b4aab9dcb4519e3b524c7887c65852bf32
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-19 17:22:00 +0400

    style(Comments): add some bg colors on comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -32,14 +32,15 @@ export const Comments = ({ postId }) => {
 
   return (
     <div>
-      <h3>These are comments</h3>
-      <ul>
+      <h3 className="text-2xl font-extrabold tracking-tight text-stone-400 ">
+        Comments
+      </h3>
+      <ul className="bg-gray-200 p-2.5 rounded-md flex flex-col gap-2.5">
         {comments.map((comment) => (
-          <li key={comment.id}>
+          <li key={comment.id} className="bg-stone-100 p-2.5">
             <h4>{comment.name}</h4>
             <p>{comment.body}</p>
             <Button
-              className="border cursor-pointer rounded-md p-2 bg-red-800"
               handleClick={() => deletePost(comment.id)}
               bgColor="tomato"
               textContent="delete"
