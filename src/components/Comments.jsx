622356042682f78c65ce3733f82ad4d0aceffa28
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-19 15:33:40 +0400

    refac(Commets): use comments?postId= instead of just /comments endpoint

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,28 +3,21 @@ import { useEffect, useState } from "react";
 
 export const Comments = ({ postId }) => {
   const [comments, setComments] = useState([]);
-  const [postComments, setPostComments] = useState([]);
 
   useEffect(() => {
     const fetchComments = async () => {
       const { data } = await axios.get(
-        "https://jsonplaceholder.typicode.com/comments",
+        `https://jsonplaceholder.typicode.com/comments?postId=${postId}`,
       );
       setComments(data);
     };
     fetchComments();
-  }, []);
-
-  useEffect(() => {
-    setPostComments(
-      comments.filter((comment) => comment.postId === Number(postId)),
-    );
-  }, [comments, postId]);
+  }, [postId]);
 
   return (
     <div>
       <h3>These are comments</h3>
-      {postComments.map((comment) => (
+      {comments.map((comment) => (
         <div key={comment.id}>
           <h4>{comment.name}</h4>
           <p>{comment.body}</p>
