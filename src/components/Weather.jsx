860a468ec5c38327ed2f272ace9f39154a84b7ac
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-22 16:52:08 +0400

    feat(Weather): add bg for wheather component

---
+++ src/components/Weather.jsx
@@ -0,0 +1,7 @@
+export default function Weather() {
+  return (
+    <div className="h-full w-full bg-[url(assets/weather.jpg)] bg-cover bg-center">
+      This is a weather app
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
+import Weather from "../components/Weather";
+
 export default function Home() {
   return (
-    <div className="w-full h-full bg-stone-300 py-10">
-      <div className="grid w-fit grid-cols-1 md:grid-cols-2 gap-4 py-10 mx-auto">
-        This is a home page
-      </div>
+    <div className="w-full h-full bg-stone-300 ">
+      <Weather />
     </div>
   );
 }
