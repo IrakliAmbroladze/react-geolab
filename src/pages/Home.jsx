19e1298735c9f68c8846846cf4c73ccf38462032
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-22 15:19:05 +0400

    feat(Blog): move posts content from home to posts route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,8 @@
-import { useEffect, useState } from "react";
-import Card from "../components/Card.jsx";
-
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://dummyjson.com/posts");
-        const data = await response.json();
-        setPosts(data.posts);
-      } catch (error) {
-        console.log("error is ", error);
-      }
-    };
-    fetchData();
-  }, []);
   return (
     <div className="w-full h-full bg-stone-300 py-10">
       <div className="grid w-fit grid-cols-1 md:grid-cols-2 gap-4 py-10 mx-auto">
-        {posts.map((p) => (
-          <Card key={p.id} product={p} detailsBtn={true} />
-        ))}
+        This is a home page
       </div>
     </div>
   );
---
+++ src/pages/Posts.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Card from "../components/Card.jsx";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/posts");
+        const data = await response.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.log("error is ", error);
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <div className="w-full h-full bg-stone-300 py-10">
+      <div className="grid w-fit grid-cols-1 md:grid-cols-2 gap-4 py-10 mx-auto">
+        {posts.map((p) => (
+          <Card key={p.id} product={p} detailsBtn={true} />
+        ))}
+      </div>
+    </div>
+  );
+}
