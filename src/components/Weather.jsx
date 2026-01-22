f7227d8a47a809dfc8774060cf99cc617d76354c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-22 17:28:39 +0400

    feat(Weather): add search input and button

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,7 +1,19 @@
+import { Button } from "./Button";
+
 export default function Weather() {
   return (
-    <div className="h-full w-full bg-[url(assets/weather.jpg)] bg-cover bg-center">
-      This is a weather app
+    <div className="flex justify-center items-center h-full w-full bg-[url(assets/weather.jpg)] bg-cover bg-center">
+      <div className="max-w-4xl w-full text-white">
+        <div className="flex gap-10 ">
+          <input
+            type="text "
+            placeholder="Enter city ..."
+            className="flex-1 border white rounded-md p-2.5"
+          />
+          <Button textContent="search" bgColor="#A9A9A9" />
+        </div>
+        <div className="min-h-[400px]"></div>
+      </div>
     </div>
   );
 }
