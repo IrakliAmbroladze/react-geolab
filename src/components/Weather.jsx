d8b327998af7a9dc8aff28a761c82881c927d536
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-23 13:41:59 +0400

    feat(Weather): add a loader before an initial render

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 export default function Weather() {
   const [currentCity, setCurrentCity] = useState("Tbilisi");
   const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
   const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const { VITE_WEATHER_API_KEY } = import.meta.env;
 
@@ -28,6 +29,7 @@ export default function Weather() {
           },
         });
         setWeather(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.log(error);
         setWeather(null);
@@ -35,7 +37,6 @@ export default function Weather() {
     };
     fetchWeather();
   }, [currentCity, VITE_WEATHER_API_KEY]);
-  console.log(weather);
 
   const handleSearch = () => {
     setCurrentCity(searchRef.current.value);
@@ -60,36 +61,51 @@ export default function Weather() {
           />
         </div>
         <div className="min-h-[400px] border max-w-4xl w-full text-white bg-white/10 backdrop-blur-xs mt-[40px] rounded-md flex flex-col justify-between px-5 py-2">
-          <div className="flex justify-between items-center min-h-[100px]">
-            <h2 className="text-3xl">
-              {weather?.name || "Please enter a valid city name"}
-            </h2>
-            <h3>
-              {weather ? (
-                <img
-                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                  alt="icon"
-                />
-              ) : (
-                <Cloud className="w-[100px] h-[100px] p-[20px]" />
-              )}
-            </h3>
-          </div>
-          <div className="flex gap-5 items-center">
-            <h2 className="text-5xl font-bold">
-              {Math.round(weather?.main.temp) || "--"}°
-            </h2>
-            <h3 className="text-4xl">{weather?.weather[0].main || "----"}</h3>
-          </div>
-          <div className=" flex justify-between gap-4">
-            {weatherData.map((item) => (
-              <WeatherCard
-                title={item.title}
-                result={item.result}
-                key={item.title}
+          {isLoading ? (
+            <div className="w-full flex items-center justify-center">
+              <img
+                src={"/assets/loading.gif"}
+                alt="loading..."
+                width={50}
+                height={50}
               />
-            ))}
-          </div>
+            </div>
+          ) : (
+            <>
+              <div className="flex justify-between items-center min-h-[100px]">
+                <h2 className="text-3xl">
+                  {weather?.name || "Please enter a valid city name"}
+                </h2>
+                <h3>
+                  {weather ? (
+                    <img
+                      src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                      alt="icon"
+                    />
+                  ) : (
+                    <Cloud className="w-[100px] h-[100px] p-[20px]" />
+                  )}
+                </h3>
+              </div>
+              <div className="flex gap-5 items-center">
+                <h2 className="text-5xl font-bold">
+                  {Math.round(weather?.main.temp) || "--"}°
+                </h2>
+                <h3 className="text-4xl">
+                  {weather?.weather[0].main || "----"}
+                </h3>
+              </div>
+              <div className=" flex justify-between gap-4">
+                {weatherData.map((item) => (
+                  <WeatherCard
+                    title={item.title}
+                    result={item.result}
+                    key={item.title}
+                  />
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
