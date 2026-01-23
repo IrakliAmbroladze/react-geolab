f458ecccaae3b6f3577508f87d47baa3acb5f407
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-23 14:04:32 +0400

    feat(Weather): use form for search to trigger submit on keyboard ENTER

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -53,19 +53,23 @@ export default function Weather() {
         <h1 className="text-3xl font-bold text-center mb-5">
           Today's Weather Forecast
         </h1>
-        <div className="flex gap-10 ">
+        <form className="flex gap-10 ">
           <input
             type="text"
             placeholder="Enter city ..."
-            className="flex-1 border border-white rounded-md p-2.5 text-white"
+            className="flex-1 border border-white rounded-md p-2.5 text-white focus:outline-none focus:shadow-lg"
             ref={searchRef}
           />
           <Button
+            type="submit"
             textContent="search"
             bgColor="#A9A9A9"
-            handleClick={handleSearch}
+            handleClick={(e) => {
+              e.preventDefault();
+              handleSearch();
+            }}
           />
-        </div>
+        </form>
         <div className="min-h-[400px] border max-w-4xl w-full text-white bg-white/10 backdrop-blur-xs mt-[40px] rounded-md flex flex-col justify-between px-5 py-2">
           {isLoading ? (
             <div className="w-full flex items-center justify-center">
