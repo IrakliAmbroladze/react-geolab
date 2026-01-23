156c3b7e2637e54b1305c82ed408d26c0020d5f5
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-23 13:02:48 +0400

    style(WeatherCard): update bg color

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,6 +1,6 @@
 export const WeatherCard = ({ title, result }) => {
   return (
-    <div className="border rounded-md flex-1 p-3">
+    <div className="bg-white/10 rounded-md flex-1 p-3">
       {title}
       <h2>{result}</h2>
     </div>
