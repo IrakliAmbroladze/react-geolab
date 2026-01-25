030d3308f704c5e923822b4241f898d0d5000051
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-25 19:13:13 +0400

    fix(Modal): added z-50 index to fix card overlapping issue on Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,6 @@
 export const Modal = ({ children }) => {
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+    <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
       <div className="bg-white max-w-[500px] w-full p-4 rounded-md">
         {children}
       </div>
