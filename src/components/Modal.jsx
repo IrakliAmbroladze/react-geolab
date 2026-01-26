d7c7789852ae727ff7a1dffc2d55f334912a7216
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-26 12:37:48 +0400

    feat(Modal): moved close button from PostEditingModal into a Modal for default purposes

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,13 @@
-export const Modal = ({ children }) => {
+import { Button } from "./Button";
+
+export const Modal = ({ children, handleClose = () => {} }) => {
   return (
-    <div className="fixed inset-0 bg-black/60 flex items-center justify-center z-50">
-      <div className="bg-white max-w-[500px] w-full p-4 rounded-md">
-        {children}
+    <div className="fixed inset-0 bg-black/60 flex flex-col items-center justify-center z-50">
+      <div className="bg-white max-w-125 w-full p-4 rounded-md">
+        <div className="text-end">
+          <Button textContent="X" bgColor="tomato" handleClick={handleClose} />
+        </div>
+        <div>{children}</div>
       </div>
     </div>
   );
--- src/components/PostEditingModal.jsx
+++ src/components/PostEditingModal.jsx
@@ -1,11 +1,5 @@
-import { Button } from "./Button";
 import { Modal } from "./Modal";
 
 export const PostEditingModal = ({ handleBtnClick = () => {} }) => {
-  return (
-    <Modal>
-      test modal
-      <Button textContent="close" bgColor="gray" handleClick={handleBtnClick} />
-    </Modal>
-  );
+  return <Modal handleClose={handleBtnClick}>test modal</Modal>;
 };
