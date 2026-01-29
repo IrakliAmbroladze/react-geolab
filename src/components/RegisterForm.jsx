1d44226dc4e3049eb5a8e828d4f45edff882dc2a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-29 10:23:30 +0400

    feat(RegisterForm): added initial handleSubmit function

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,11 +1,20 @@
+import { Button } from "./Button";
 import { Input } from "./Input";
 
 export const RegisterForm = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
   return (
-    <form className="flex flex-col gap-2 max-w-96 w-full">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 max-w-96 w-full px-2.5"
+    >
       <Input type="email" placeholder="Email" />
       <Input type="text" placeholder="Username" />
       <Input type="password" placeholder="Password" />
+      <Button type="submit" textContent="submit" />
     </form>
   );
 };
