4c129b5cf6f31c0ae015dc34f39743b7aeeb1d7d
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-29 12:14:09 +0400

    feat(RegisterForm): added validateInput function for validation

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -8,11 +8,43 @@ export const RegisterForm = () => {
     username: "",
     password: "",
   });
+
+  const [validationErrors, setValidationErrors] = useState({
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const validateInput = (name, value) => {
+    switch (name) {
+      case "email":
+        if (value === "") return "Email is required";
+        else if (!value.includes("@")) return "Email must include `@`";
+        else if (!value.includes(".")) return "Email must include `.`";
+        else return "";
+      case "username":
+        if (value === "") return "Username is required";
+        else if (value.length < 4)
+          return "Username must be at least 4 characters long";
+        else return "";
+      case "password":
+        if (value === "") return "Password is required";
+        else if (value.length < 8)
+          return "Password must be at least 8 characters long";
+        else return "";
+      default:
+        return "";
+    }
+  };
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
       [name]: value,
+    }));
+    setValidationErrors((prev) => ({
+      ...prev,
+      [name]: validateInput(name, value),
     }));
     console.log(name);
     console.log(value);
