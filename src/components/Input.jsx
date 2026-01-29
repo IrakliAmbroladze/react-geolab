6b5c19a70a38fb702fd6851428730a81a9ade04e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-29 10:27:24 +0400

    feat(RegisterForm): added initial formData state

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,13 @@
-export const Input = ({ type = "text", placeholder = "Enter text ... " }) => {
+export const Input = ({
+  type = "text",
+  placeholder = "Enter text ... ",
+  name = "",
+}) => {
   return (
     <input
       type={type}
       placeholder={placeholder}
+      name={name}
       className="w-full py-3 px-3.5 border-2 border-[#d0d7de] rounded-lg outline-0 bg-white focus:border-[#5d9eff] focus:shadow-lg transition-all duration-150 ease-in-out"
     />
   );
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import { Button } from "./Button";
 import { Input } from "./Input";
 
 export const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    password: "",
+  });
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("submitted");
+    console.log(formData);
   };
   return (
     <form
       onSubmit={handleSubmit}
       className="flex flex-col gap-2 max-w-96 w-full px-2.5"
     >
-      <Input type="email" placeholder="Email" />
-      <Input type="text" placeholder="Username" />
-      <Input type="password" placeholder="Password" />
+      <Input type="email" placeholder="Email" name="email" />
+      <Input type="text" placeholder="Username" name="username" />
+      <Input type="password" placeholder="Password" name="password" />
       <Button type="submit" textContent="submit" />
     </form>
   );
