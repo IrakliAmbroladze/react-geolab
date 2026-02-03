6923dd26d085293b9ab4e10c3349fbb9386047db
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-04 00:23:40 +0400

    refac(register): extracted error message into Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,30 @@
+const InputError = ({ children }) => {
+  return (
+    <p className="text-red-500 text-[12px] px-2 absolute -top-2.5 left-5 bg-white">
+      {children}
+    </p>
+  );
+};
+
 export const Input = ({
   type = "text",
   placeholder = "Enter text ... ",
   name = "",
   required = false,
   register,
+  errorMessage = "",
 }) => {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      name={name}
-      className="w-full py-3 px-3.5 border-2 border-[#d0d7de] rounded-lg outline-0 bg-white focus:border-[#5d9eff] focus:shadow-lg transition-all duration-150 ease-in-out"
-      required={required}
-      {...register(name)}
-    />
+    <div className="relative">
+      <input
+        type={type}
+        placeholder={placeholder}
+        name={name}
+        className="w-full py-3 px-3.5 border-2 border-[#d0d7de] rounded-lg outline-0 bg-white focus:border-[#5d9eff] focus:shadow-lg transition-all duration-150 ease-in-out"
+        required={required}
+        {...register(name)}
+      />
+      {errorMessage && <InputError>{errorMessage}</InputError>}
+    </div>
   );
 };
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -27,60 +27,35 @@ export const RegisterForm = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col gap-5 mb-5"
       >
-        <div className="relative">
-          <Input
-            type="email"
-            placeholder="Email"
-            name="email"
-            register={register}
-          />
-          {errors.email && (
-            <p className="text-red-500 text-[12px] px-2 absolute -top-2.5 left-5 bg-white">
-              {errors.email.message}
-            </p>
-          )}
-        </div>
-        <div className="relative">
-          <Input
-            type="text"
-            placeholder="Username"
-            name="username"
-            register={register}
-          />
-          {errors.username && (
-            <p className="text-red-500 text-[12px] px-2 absolute -top-2.5 left-5 bg-white">
-              {errors.username.message}
-            </p>
-          )}
-        </div>
-        <div className="relative">
-          <Input
-            type="password"
-            placeholder="Password"
-            name="password"
-            register={register}
-          />
-          {errors.password && (
-            <p className="text-red-500 text-[12px] px-2 absolute -top-2.5 left-5 bg-white">
-              {errors.password.message}
-            </p>
-          )}
-        </div>
-        <div className="relative">
-          <Input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirm_password"
-            register={register}
-          />
-
-          {errors.confirm_password && (
-            <p className="text-red-500 text-[12px] px-2 absolute -top-2.5 left-5 bg-white">
-              {errors.confirm_password.message}
-            </p>
-          )}
-        </div>
-        <Button type="submit" />
+        <Input
+          type="email"
+          placeholder="Email"
+          name="email"
+          register={register}
+          errorMessage={errors.email?.message}
+        />
+        <Input
+          type="text"
+          placeholder="Username"
+          name="username"
+          register={register}
+          errorMessage={errors.username?.message}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+          errorMessage={errors.password?.message}
+        />
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          name="confirm_password"
+          register={register}
+          errorMessage={errors.confirm_password?.message}
+        />
+        <Button type="submit" textContent="submit" />
       </form>
     </div>
   );
