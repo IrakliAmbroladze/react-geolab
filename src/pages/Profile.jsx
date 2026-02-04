b2948b5bb66c3e4f2e9da56f60ad97e509a9e358
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-04 23:09:40 +0400

    feat(Square): added cunter for testing if Square component rerenders when count is changed

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { Square } from "../components/square";
+import { Button } from "../components/Button";
 
 export default function Profile() {
   const [count, setCount] = useState(0);
   return (
-    <div className="w-full h-full grid place-content-center">
-      count is: {count}
-      <button className="border " onClick={() => setCount((prev) => prev + 1)}>
-        გაზრდა
-      </button>
+    <div className="w-full h-full flex justify-center items-center flex-col">
+      <div className="flex gap-4 items-center">
+        count is: {count}
+        <Button
+          textContent="increase"
+          handleClick={() => setCount((prev) => prev + 1)}
+        >
+          გაზრდა
+        </Button>
+      </div>
       <Square />
     </div>
   );
