3b2dcb3d84026b59b2363b153631e775d7e28676
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-04 23:18:05 +0400

    feat(Square): added Red Green and Blue buttons

--- src/components/square.jsx
+++ src/components/square.jsx
@@ -1,4 +1,15 @@
+import { Button } from "./Button";
+
 export const Square = () => {
   console.log("render Square component");
-  return <div>This is a square component</div>;
+  return (
+    <div>
+      <div>square</div>
+      <div className="flex gap-2.5">
+        <Button textContent="Red" bgColor="red" />
+        <Button textContent="Green" bgColor="green" />
+        <Button textContent="Blue" bgColor="blue" />
+      </div>
+    </div>
+  );
 };
