fb4526223151ddd5b114066ed408ad23ae4662e1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-02-12 16:01:33 +0400

    feat(Login): form is done till sending data to the backend

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,42 @@
+import { useForm } from "react-hook-form";
 import { Form } from "../components/Form";
 import { Input } from "../components/Input";
 
 export default function Login() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit");
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm();
+  const onSubmit = async (data) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log("submitted data", data);
+    reset({
+      username: "",
+      password: "",
+    });
   };
   return (
     <div className="center h-full">
-      <Form title="Login" hasSubmitButton onSubmit={handleSubmit}>
-        <Input type="text" placeholder="Username" name="username" />
-        <Input type="password" placeholder="Password" name="password" />
+      <Form
+        isSubmitting={isSubmitting}
+        title="Login"
+        hasSubmitButton
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input
+          type="text"
+          placeholder="Username"
+          name="username"
+          register={register}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          name="password"
+          register={register}
+        />
       </Form>
       ;
     </div>
